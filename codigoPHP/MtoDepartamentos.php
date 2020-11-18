87c999d30349069d86c8ee4e2d27f6803817216e
Rodrigmen <roblesminambresrodrigo@gmail.com>
2020-11-18 12:58:30 +0100

    Add bajaLogica and rehabilitar and update MtoDepartamentos and editar

--- codigoPHP/MtoDepartamentos.php
+++ codigoPHP/MtoDepartamentos.php
@@ -21,10 +21,10 @@
 
         <?php
         /**
-         * Formulario de búsqueda de departamentos por descripción (por una parte del campo DescDepartamento, si el usuario no pone nada deben aparecer todos los departamentos) [PDO]
+         * Página principal de Mantenimiento de Departamentos
          * 
          * @version 1.0.0
-         * @since 29-10-2020
+         * @since 18-11-2020
          * @author Rodrigo Robles <rodrigo.robmin@educa.jcyl.es>
          */
         require_once '../config/confDBPDO.php';
@@ -34,48 +34,14 @@
             $oConexionPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //le damos este atributo a la conexión (la configuramos) para poder utilizar las excepciones
             //Requerimos una vez la libreria de validaciones
             require_once '../core/201020libreriaValidacion.php';
-
-            //Creamos una variable boleana para definir cuando esta bien o mal rellenado el formulario
-            $entradaOK = true;
-
-            //Creamos dos constantes: 'REQUIRED' indica si un campo es obligatorio (tiene que tener algun valor); 'OPTIONAL' indica que un campo no es obligatorio
-            define('REQUIRED', 1);
-            define('OPTIONAL', 0);
-
-            //Array que contiene los posibles errores de los campos del formulario
-            $aErrores = [
-                'eDescripcion' => null,
-            ];
-
-            //Array que contiene los valores correctos de los campos del formulario
-            $aFormulario = [
-                'fDescripcion' => null
-            ];
             ?>
             <form id="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
                 <fieldset>
                     <!-----------------DESCRIPCIÓN----------------->
                     <div class="required">
                         <label for="codigo">Descripción: </label>
-                        <input type="search" name="descripcion" placeholder="Departamento de..." value="<?php
-                        //si no hay error y se ha insertado un valor en el campo con anterioridad
-                        if ($aErrores['eDescripcion'] == null && isset($_POST['descripcion'])) {
-
-                            //se muestra dicho valor (el campo no aparece vacío si se relleno correctamente 
-                            //[en el caso de que haya que se recarge el formulario por un campo mal rellenado, asi no hay que rellenarlo desde 0])
-                            echo $_POST['descripcion'];
-                        }
-                        ?>"/><input type="submit" name="buscar" value="Buscar" />
-
-                        <?php
-                        //si hay error en este campo
-                        if ($aErrores['eDescripcion'] != NULL) {
-                            echo "<div class='errores'>" .
-                            //se muestra dicho error
-                            $aErrores['eDescripcion'] .
-                            '</div>';
-                        }
-                        ?>
+                        <input type="search" name="descripcion" placeholder="Departamento de..." value="<?php if (isset($_POST['descripcion'])) {echo $_POST['descripcion'];} ?>"/>
+                        <input type="submit" name="buscar" value="Buscar" />
                     </div>         
                     <a href="altaDepartamento.php"> <input type="button" name="Insertar Departemento" value="Insertar Departemento"></a>
                     <a href="mostrarCodigo.php"> <input type="button" name="Ver Código" value="Ver código"></a>
@@ -83,101 +49,24 @@
             </form>
 
             <?php
-            if (isset($_POST['buscar'])) { //si se pulsa 'enviar' (input name="enviar")
-                //Validación de los campos (el resultado de la validación se mete en el array aErrores para comprobar posteriormente si da error)
-                //DESCRIPCIÓN (input type="text") [OBLIGATORIO {texto alfabetico}] 
-                $aErrores['eDescripcion'] = validacionFormularios::comprobarAlfaNumerico($_POST['descripcion'], 35, 1, OPTIONAL);
-
-
-
-                //recorremos el array de posibles errores (aErrores), si hay alguno, el campo se limpia y entradaOK es falsa (se vuelve a cargar el formulario)
-                foreach ($aErrores as $campo => $validacion) {
-                    if ($validacion != null) {
-                        $entradaOK = false;
-                    }
-                }
-            } else { // sino se pulsa 'enviar'
-                $entradaOK = false;
+            $descripcionBuscada = "";
+            if (isset($_POST['buscar'])) {
+                $descripcionBuscada = $_POST['descripcion'];
             }
 
-            if ($entradaOK) { //si el formulario esta bien rellenado
-                //formulario, se vuelve a mostrar (es el buscador), por si el usuario quiere seguir buscando (buscador constante)
-                //Consulta preparada = búsqueda de departamento
-                //Preparación
-                $consultaBuscar = "SELECT * FROM Departamento WHERE DescDepartamento LIKE CONCAT('%', :descripcion, '%')";
+            $consultaBuscar = "SELECT * FROM Departamento WHERE DescDepartamento LIKE CONCAT('%', :descripcion, '%')";
 
-                $buscarDepartamento = $oConexionPDO->prepare($consultaBuscar);
+            $buscarDepartamento = $oConexionPDO->prepare($consultaBuscar);
 
-                //Inserción de datos en al consulta
-                $buscarDepartamento->bindParam(':descripcion', $_POST['descripcion']);
+            //Inserción de datos en al consulta
+            $buscarDepartamento->bindParam(':descripcion', $descripcionBuscada);
 
-                //Ejecución
-                $buscarDepartamento->execute();
+            //Ejecución
+            $buscarDepartamento->execute();
 
-                $numeroDepartamentos = $buscarDepartamento->rowCount(); //número de departamentos encontrados
+            $numeroDepartamentos = $buscarDepartamento->rowCount(); //número de departamentos encontrados
 
-                if ($numeroDepartamentos !== 0) { //si si se encuentran departamentos, se muestran
-                    echo "<table>"
-                    . "<thead>"
-                    . "<tr>"
-                    . "<th>Código</th>"
-                    . "<th>Descripción</th>"
-                    . "<th>Volumen de negocio</th>"
-                    . "<th>Fecha de baja</th>"
-                    . "<th>Opciones</th>"
-                    . "</tr>"
-                    . "</thead>"
-                    . "<tbody>";
-                    while ($departamento = $buscarDepartamento->fetch(PDO::FETCH_OBJ)) {
-                        $codigoDep = $departamento->CodDepartamento;
-                        $descDep = $departamento->DescDepartamento;
-                        $volDep = $departamento->VolumenNegocio;
-                        $fechaDep = $departamento->FechaBaja;
-                        echo "<tr>"
-                        . "<td>$codigoDep</td>"
-                        . "<td>$descDep</td>"
-                        . "<td>$volDep</td>";
-                        if (is_null($fechaDep)) {
-                            echo "<td>Activo</td>";
-                        } else {
-                            echo "<td>$fechaDep</td>";
-                        }
-                        ?>
-                    <td>
-                        <a href="editarDepartamento.php?codigoDep=<?php echo $codigoDep ?>">
-                            <img class="imgejer" src="../webroot/css/images/editar.png"  alt="Editar" title="Editar"/>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="mostrarDepartamento.php?codigoDep=<?php echo $codigoDep ?>">
-                            <img class="imgejer" src="../webroot/css/images/analitica.png"alt="Ver datos" title="Ver datos"/>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="bajaDepartamento.php?codigoDep=<?php echo $codigoDep ?>">
-                            <img class="imgejer" src="../webroot/css/images/eliminar.png"alt="Eliminar" title="Eliminar"/>
-                        </a>
-                    </td>
-                    <?php
-                    echo "</tr>";
-                }
-
-                echo "</tbody>"
-                . "</table>";
-            } else {  //si no encontramos ningún departamento, lo notificamos al usuario
-                echo "<h4>¡No hay ningún departamento con esa descripción!</h4>";
-            }
-        } else { // si el formulario no esta correctamente rellenado (campos vacios o valores introducidos incorrectos) o no se ha rellenado nunca
-            //MOSTRAMOS TODOS LOS DEPARTAMENTOS EN UNA TABLA, SITUADA POR DEBAJO DEL BUSCADOR
-            //Preparamos la consulta
-            $consultaTodos = "SELECT * FROM Departamento ORDER BY CodDepartamento";
-            $seleccionTodosDep = $oConexionPDO->prepare($consultaTodos);
-
-            //Ejecutamos la consulta
-            $seleccionTodosDep->execute();
-
-            $numeroDepartamentos = $seleccionTodosDep->rowCount();
-            if ($numeroDepartamentos !== 0) { //comprobamos si hay departamentos
+            if ($numeroDepartamentos !== 0) { //si si se encuentran departamentos, se muestran
                 echo "<table>"
                 . "<thead>"
                 . "<tr>"
@@ -189,7 +78,8 @@
                 . "</tr>"
                 . "</thead>"
                 . "<tbody>";
-                while ($departamento = $seleccionTodosDep->fetch(PDO::FETCH_OBJ)) {
+                while ($departamento = $buscarDepartamento->fetch(PDO::FETCH_OBJ)) {
+                    $habilitado = true;
                     $codigoDep = $departamento->CodDepartamento;
                     $descDep = $departamento->DescDepartamento;
                     $volDep = $departamento->VolumenNegocio;
@@ -202,32 +92,49 @@
                         echo "<td>Activo</td>";
                     } else {
                         echo "<td>$fechaDep</td>";
+                        $habilitado = false;
                     }
                     ?>
-                    <td>
-                        <a href="editarDepartamento.php?codigoDep=<?php echo $codigoDep ?>">
-                            <img class="imgejer" src="../webroot/css/images/editar.png"  alt="Editar" title="Editar"/>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="mostrarDepartamento.php?codigoDep=<?php echo $codigoDep ?>">
-                            <img class="imgejer" src="../webroot/css/images/analitica.png"alt="Ver datos" title="Ver datos"/>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="bajaDepartamento.php?codigoDep=<?php echo $codigoDep ?>">
-                            <img class="imgejer" src="../webroot/css/images/eliminar.png"alt="Eliminar" title="Eliminar"/>
-                        </a>
-                    </td>
+                <td>
+                    <a href="editarDepartamento.php?codigoDep=<?php echo $codigoDep ?>">
+                        <img class="imgejer" src="../webroot/css/images/editar.png"  alt="Editar" title="Editar"/>
+                    </a>
+                </td>
+                <td>
+                    <a href="mostrarDepartamento.php?codigoDep=<?php echo $codigoDep ?>">
+                        <img class="imgejer" src="../webroot/css/images/analitica.png"alt="Ver datos" title="Ver datos"/>
+                    </a>
+                </td>
+                <td>
+                    <a href="bajaDepartamento.php?codigoDep=<?php echo $codigoDep ?>">
+                        <img class="imgejer" src="../webroot/css/images/eliminar.png"alt="Eliminar" title="Eliminar"/>
+                    </a>
+                </td>
+                <?php
+                if ($habilitado) {
+                    echo '<td>';
+                    ?>
+                    <a href="bajaLogicaDepartamento.php?codigoDep=<?php echo $codigoDep ?>">
+                        <img class="imgejer" src="../webroot/css/images/habilitar.png"  alt="Inhabilitar" title="Inhabilitar"/>
+                    </a>
                     <?php
-                    echo "</tr>";
+                    echo '</td>';
+                } else {
+                    echo '<td>';
+                    ?>
+                    <a href="rehabilitarDepartamento.php?codigoDep=<?php echo $codigoDep ?>">
+                        <img class="imgejer" src="../webroot/css/images/inhabilitar.png"  alt="HAbilitar" title="Habilitar"/>
+                    </a>
+                    <?php
+                    echo '</td>';
                 }
-
-                echo "</tbody>"
-                . "</table>";
-            } else {
-                echo "<h4>¡No hay ningún departamento!</h4>";
+                echo "</tr>";
             }
+
+            echo "</tbody>"
+            . "</table>";
+        } else {  //si no encontramos ningún departamento, lo notificamos al usuario
+            echo "<h4>¡No hay ningún departamento con esa descripción!</h4>";
         }
     } catch (PDOException $excepcionPDO) {
         echo "<p style='color:red;'>Mensaje de error: " . $excepcionPDO->getMessage() . "</p>"; //Muestra el mesaje de error
--- codigoPHP/bajaLogicaDepartamento.php
+++ codigoPHP/bajaLogicaDepartamento.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>EditarDepartamento - MTO</title>
+        <title>InhabilitarDepartamento - MTO</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="../webroot/css/styleOptions.css" rel="stylesheet" type="text/css"/>
@@ -16,7 +16,11 @@
             $oConexionPDO = new PDO(DSN, USER, PASSWORD, CHARSET); //creo el objeto PDO con las constantes iniciadas en el archivo datosBD.php
             $oConexionPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             require_once '../core/201020libreriaValidacion.php';
-
+            
+            $oFecha = new DateTime(); //creación del objeto (Representación de la fecha y la hora.)
+            $oFecha->setTimezone(new DateTimeZone('Europe/Madrid')); //establecemos su zona horaria 
+            $oFechaSalida = $oFecha->format('Y-m-d'); //Devuelve la fecha formateada según el formato dado
+                        
             /* SACAR A TRAVÉS DE UNA CONSULTA */
             $consultaBuscar = "SELECT * FROM Departamento WHERE CodDepartamento LIKE :codigo";
 
@@ -42,15 +46,12 @@
 
             //Array que contiene los posibles errores de los campos del formulario
             $aErrores = [
-                'eDescripcion' => null,
-                'eVolumen' => null
+                'eFecha' => null
             ];
-            if (isset($_POST['editar'])) {
+            if (isset($_POST['inhabilitar'])) {
 
-                //DESCRIPCIÓN (input type="text") [OBLIGATORIO {texto alfabetico}] 
-                $aErrores['eDescripcion'] = validacionFormularios::comprobarAlfaNumerico($_POST['descripcion'], 35, 1, REQUIRED);
-                //VOLUMEN DE NEGOCIO (input type="number") [OBLIGATORIO {número entero}] 
-                $aErrores['eVolumen'] = validacionFormularios::comprobarEntero($_POST['volumen'], PHP_INT_MAX, 1, OPTIONAL);
+                //FECHA DE NACIMIENTO (input type="date") [OBLIGATORIO {formato: dia/mes/año}]
+                $aErrores['eFecha']= validacionFormularios::validarFecha($_POST['fecha'], "01/01/2100", $oFechaSalida,REQUIRED);
 
                 //recorremos el array de posibles errores (aErrores), si hay alguno, el campo se limpia y entradaOK es falsa (se vuelve a cargar el formulario)
                 foreach ($aErrores as $campo => $validacion) {
@@ -63,12 +64,11 @@
             }
             if ($entradaOK) {
 
-                $consultaActualizar = "UPDATE Departamento SET DescDepartamento = :descripcion, VolumenNegocio = :volumen WHERE CodDepartamento LIKE :codigo";
-
+                $consultaActualizar = "UPDATE Departamento SET FechaBaja = :fecha WHERE CodDepartamento LIKE :codigo";
+                
                 $actualizarDepartamento = $oConexionPDO->prepare($consultaActualizar);
                 //Inserción de datos en al consulta
-                $actualizarDepartamento->bindParam(':descripcion', $_POST["descripcion"]);
-                $actualizarDepartamento->bindParam(':volumen', $_POST["volumen"]);
+                $actualizarDepartamento->bindParam(':fecha', $_POST["fecha"]);
                 $actualizarDepartamento->bindParam(':codigo', $_GET['codigoDep']);
 
                 //Ejecución
@@ -80,73 +80,59 @@
                 ?>
                 <form id="formulario" action="<?php echo $_SERVER['PHP_SELF'] . '?codigoDep=' . $_GET['codigoDep']; ?>" method="post"> 
                     <fieldset>
-                        <legend>Editar Departamento</legend>
+                        <legend>Inhabilitar Departamento</legend>
 
+                        <!-----------------CÓDIGO----------------->
                         <div class="required">
                             <label for="codigo">Código:</label>
                             <input type="text" name="codigo" value="<?php
                             echo $_GET["codigoDep"];
                             ?>" readonly/>
                         </div>
+
                         <!-----------------DESCRIPCIÓN----------------->
-                        <div class="optional">
+                        <div class="required">
                             <label for="codigo">Descripción: </label>
                             <input type="text" name="descripcion" placeholder="Departamento de..." value="<?php
-                            //si no hay error y se ha insertado un valor en el campo con anterioridad
-                            if ($aErrores['eDescripcion'] == null && isset($_POST['descripcion'])) {
-
-                                //se muestra dicho valor (el campo no aparece vacío si se relleno correctamente 
-                                //[en el caso de que haya que se recarge el formulario por un campo mal rellenado, asi no hay que rellenarlo desde 0])
-                                echo $_POST['descripcion'];
-                            } else {
-                                echo $descDep;
-                            }
-                            ?>"/>
-                                   <?php
-                                   //si hay error en este campo
-                                   if ($aErrores['eDescripcion'] != NULL) {
-                                       echo "<div class='errores'>" .
-                                       //se muestra dicho error
-                                       $aErrores['eDescripcion'] .
-                                       '</div>';
-                                   }
-                                   ?>
+                            echo $descDep;
+                            ?>" readonly/>
                         </div>
+
                         <!-----------------VOLUMEN DE NEGOCIO----------------->
-                        <div class="optional">
+                        <div class="required">
                             <label for="volumen">Volumen:</label>
                             <input type="number" name="volumen"  value="<?php
-                            //si no hay error y se ha insertado un valor en el campo con anterioridad
-                            if ($aErrores['eVolumen'] == null && isset($_POST['volumen'])) {
-                                //se muestra dicho valor (el campo no aparece vacío si se relleno correctamente 
-                                //[en el caso de que haya que se recarge el formulario por un campo mal rellenado, asi no hay que rellenarlo desde 0])
-                                echo $_POST['volumen'];
-                            } else {
-                                echo $volDep;
-                            }
-                            ?>"/>
+                            echo $volDep;
+                            ?>" readonly/>
+                        </div>
+                        
+                        <!-----------------FECHA DE NACIMIENTO----------------->
+                        <div class="required">
+                            <label for="fecha">Fecha de Baja:</label>
+                            <input type="date" name="fecha" value="<?php 
+                                //si no hay error y se ha insertado un valor en el campo con anterioridad
+                                if(is_null($aErrores['eFecha']) && isset($_POST['fecha'])){ 
 
-                            <?php
-                            //si hay error en este campo
-                            if ($aErrores['eVolumen'] != NULL) {
-                                echo "<div class='errores'>" .
-                                //se muestra dicho error
-                                $aErrores['eVolumen'] .
-                                '</div>';
-                            }
+                                    //se muestra dicho valor (el campo no aparece vacío si se relleno correctamente 
+                                    //[en el caso de que haya que se recarge el formulario por un campo mal rellenado, asi no hay que rellenarlo desde 0])
+                                    echo $_POST['fecha'];   
+                                }else{
+                                    echo $oFechaSalida;
+                                }
+                            ?>"/>
+                                                    
+                            <?php 
+                                //si hay error en este campo
+                                if ($aErrores['eFecha'] != NULL) {
+                                    echo "<div class='errores'>".
+                                        //se muestra dicho error
+                                        $aErrores['eFecha']. 
+                                    '</div>'; 
+                                }
                             ?>
                         </div>
+
                         <?php
-                        if (!empty($fechaDep)) {
-                            ?>
-                            <div class="required">
-                                <label for="nombre">Fecha de Baja:</label>
-                                <input type="text" name="fecha"  value="<?php
-                                echo $fechaDep;
-                                ?>" readonly/>
-                            </div>
-                            <?php
-                        }
                     }
                 } catch (PDOException $excepcionPDO) {
                     echo "<p style='color:red;'>Mensaje de error: " . $excepcionPDO->getMessage() . "</p>"; //Muestra el mesaje de error
@@ -156,7 +142,7 @@
                     unset($oConexionPDO); //destruimos el objeto  
                 }
                 ?>
-                <input type="submit" name="editar" value="Actualizar"/>
+                <input type="submit" name="inhabilitar" value="Inhabilitar"/>
                 <a class="botones" href="MtoDepartamentos.php"> <input type="button" name="cancelar" value="Cancelar"></a>           
             </fieldset>
         </form>
